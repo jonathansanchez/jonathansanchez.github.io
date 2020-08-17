ae07bdbf2b96d136f1732d23d32f9e8e495479a6
Jonathan Sánchez <jsanchezorellana@gmail.com>
2020-08-16 20:34:32 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,55 +1,26 @@
-var radioApp = {};
-radioApp.itunes = [];
-radioApp.currentImage = {};
-
-function fetch_itunes(artist, song, callback){
-	if(typeof radioApp.itunes[artist+"-"+song] !== 'undefined'){
-		callback(radioApp.itunes[artist+"-"+song]);
-	}else{
-		$.getJSON( "https://itunes.apple.com/search?term=" + encodeURIComponent(artist + " - " + song) + "&entity=song&callback=?", {
-					format: "json"
-		}).done(function( data ) {
-			radioApp.itunes[artist+"-"+song] = data;
-			callback(radioApp.itunes[artist+"-"+song]);
-		}).error(function(){
-			callback(null);
-		});
-	}
+function getData() {
+    var t = Math.round(new Date().getTime() / 1e3);
+    $.ajax({
+        url: "https://s3-eu-west-1.amazonaws.com/nme-rp-aws1-timeincuk-net/nowplaying/nme2.json?cache=" + t,
+        dataType: "jsonp",
+        crossDomain: !0,
+        success: function (t) {
+            console.log(response);
+        },
+    });
 }
+var radioApp = {
+    event: {
+        nowPlaying: function (t) {
+            console.log(t);		
+            $("#image").attr("src", t.image), $("h5").text(t.artist_text), $("p").text(t.song_text);
+        },
+    },
+};
+$(document).ready(function () {
+    setInterval(getData, 1e4);
+}),
 
-function msieversion() {
-	var ua = window.navigator.userAgent;
-	var msie = ua.indexOf("MSIE ");
-	if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./)){
-		return parseInt(ua.substring(msie + 5, ua.indexOf(".", msie)));
-	}else{
-		return false;
-	}
-	return false;
-}
-
-$(document).ready(function(){
-	setInterval(function(){ update_meta(); }, 10000);
-	var ie_version = msieversion();
-	if(ie_version !== false){
-		$('.nme-header').css('width', '100%').css('width', '-=55px');
-	}
+$(window).on("load", function () {
+    getData();
 });
-
-update_meta();
-
-function update_meta(){
-	//$.getJSON("nme2.json", function( data ) {
-	$.getJSON("https://radio.nme.com/radioplayer/api/nme2.json", function( data ) {
-		updateStationBlock('nme2', data);
-	});
-}
-
-function updateStationBlock(stat, data){
-  console.log(stat);
-  console.log(data);
-  //$("#image").attr("src", t.image), $("h5").text(t.artist_text), $("p").text(t.song_text);
-
-	$('#' + stat + '_artist').text(data.artist);
-	$('#' + stat + '_track').text(data.song);
-}
