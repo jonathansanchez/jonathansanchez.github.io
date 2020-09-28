c015197f35fe1621e0276acb0a0744d023eacc15
Jonathan Sánchez <jsanchezorellana@gmail.com>
2020-09-27 21:59:33 -0300

    Update radio.js

--- radio.js
+++ radio.js
@@ -3,62 +3,52 @@ radioApp.itunes = [];
 radioApp.currentImage = {};
 
 function fetch_itunes(artist, song, callback){
-	if(typeof radioApp.itunes[artist+"-"+song] !== 'undefined'){
-		callback(radioApp.itunes[artist+"-"+song]);
-	}else{
-		$.getJSON( "https://itunes.apple.com/search?term=" + encodeURIComponent(artist + " - " + song) + "&entity=song&callback=?", {
-					format: "json"
-		}).done(function( data ) {
-			radioApp.itunes[artist+"-"+song] = data;
-			callback(radioApp.itunes[artist+"-"+song]);
-		}).error(function(){
-			callback(null);
-		});
-	}
-}
-
-function msieversion() {
-	var ua = window.navigator.userAgent;
-	var msie = ua.indexOf("MSIE ");
-	if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./)){
-		return parseInt(ua.substring(msie + 5, ua.indexOf(".", msie)));
-	}else{
-		return false;
-	}
-	return false;
+    if(typeof radioApp.itunes[artist+"-"+song] !== 'undefined'){
+        callback(radioApp.itunes[artist+"-"+song]);
+    }else{
+        $.getJSON( "https://itunes.apple.com/search?term=" + encodeURIComponent(artist + " - " + song) + "&entity=song&callback=?", {
+            format: "json"
+        }).done(function( data ) {
+            radioApp.itunes[artist+"-"+song] = data;
+            callback(radioApp.itunes[artist+"-"+song]);
+        });
+    }
 }
 
 $(document).ready(function(){
-	setInterval(function(){ update_meta(); }, 10000);
-	var ie_version = msieversion();
-	if(ie_version !== false){
-		$('.nme-header').css('width', '100%').css('width', '-=55px');
-	}
+    setInterval(function(){ update_meta(); }, 10000);
 });
 
 update_meta();
 
 function update_meta(){
-	/*$.getJSON("https://radio.nme.com/radioplayer/api/nme2.json", function( data ) {
-		updateStationBlock('nme2', data);
-	});*/
-	$.ajax({
-		url: "https://radio.nme.com/radioplayer/api/nme2.json",
-		dataType: "json",
-		//crossDomain: !0,
-		crossDomain: true,
-		success: function (data) {
-	    		console.log(data);
-			updateStationBlock('nme2', data);
-		},
-	});
+    $.getJSON("https://cors-anywhere.herokuapp.com/https://radio.nme.com/radioplayer/api/nme2.json", function( data ) {
+        console.log("data", data);
+        updateStationBlock("radio", data);
+    });
 }
 
-function updateStationBlock(stat, data){
-  console.log(stat);
-  console.log(data);
-  //$("#image").attr("src", t.image), $("h5").text(t.artist_text), $("p").text(t.song_text);
+function updateStationBlock(stat, data) {
+    fetch_itunes(data.artist, data.song, function(itunes_data) {
+        var defaultImage = "https://is5-ssl.mzstatic.com/image/thumb/Music123/v4/b8/38/2e/b8382e6a-dfa3-9192-01df-6d0bb4b93c4a/source/100x100bb.jpg";
+        defaultImage.replace('100x100', '300x300');
 
-	$('#' + stat + '_artist').text(data.artist);
-	$('#' + stat + '_track').text(data.song);
+        if(data.artist != null && data.song != null) {
+            if(itunes_data != null && itunes_data.resultCount > 0) {
+                var image = itunes_data.results[0].artworkUrl100.replace('100x100', '300x300');
+                if(typeof radioApp.currentImage[stat] === 'undefined' || radioApp.currentImage[stat] != image){
+                    radioApp.currentImage[stat] = image;
+                    $("#image").attr("src", image);
+                }
+            } else {
+                $("#image").attr("src", defaultImage);
+            }
+            $("h5").text(data.artist);
+            $("p").text(data.song);
+        } else {
+            $("#image").attr("src", defaultImage);
+            $("h5").text("Mastodonte FM");
+            $("p").text("");
+        }
+    });
 }
