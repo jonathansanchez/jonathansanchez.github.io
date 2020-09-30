e0a25e9e1a57a05457277a892247bb1410eeeb23
Jonathan Sánchez <jsanchezorellana@gmail.com>
2020-09-30 00:20:14 -0300

    Update radio.js

--- radio.js
+++ radio.js
@@ -22,7 +22,8 @@ $(document).ready(function(){
 update_meta();
 
 function update_meta(){
-    $.getJSON("https://cors-anywhere.herokuapp.com/https://radio.nme.com/radioplayer/api/nme2.json", function( data ) {
+    //$.getJSON("https://cors-anywhere.herokuapp.com/https://radio.nme.com/radioplayer/api/nme2.json", function( data ) {
+    $.getJSON("http://mastodonte.rf.gd/consumer.php", function( data ) {
         updateStationBlock("radio", data);
     });
 }
